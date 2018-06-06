03ba82c5e8957707682948c5dd31310316b50941
Yuri Araújo <araujoyuri@edu.unifor.br>
2018-06-05 22:46:59 -0300

    MongoDB user and password added to URL

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 import os
 
 
-URI = os.environ.get('URI')
+URI = os.environ.get('MONGODB_URI')
 DEBUG = os.environ.get('DEBUG')
 PORT = os.environ.get('PORT')
 SECRET_KEY = os.environ.get('SECRET_KEY')
